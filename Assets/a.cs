725359c0c886ec9f3a747c0f9a3b09fe922e7d50
kimchanghun <kchanghun@naver.com>
2021-10-28 16:07:37 +0900

    Test 0
    Test 0

---
+++ Assets/a.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class a : MonoBehaviour
+{
+    void Start()
+    {
+        print("a"); //±‚¡∏
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/b.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class b : MonoBehaviour
+{
+    void Start()
+    {
+        print("b"); //±‚¡∏
+    }
+
+    void Update()
+    {
+        
+    }
+}
